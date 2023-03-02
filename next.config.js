fdd5bc78161906020a8adbd250cfae2b35ff0a1b
James Stuart Milne <jstuartmilne@gmail.com>
2023-03-02 10:36:21 -0800

    use env variable

--- next.config.js
+++ next.config.js
@@ -22,6 +22,42 @@ const nextConfig = {
     conductor: {
       keyId: process.env.KEY,
       keySecret: process.env.SECRET,
+      serverUrl: `http://${NEXT_PUBLIC_VERCEL_URL}/api`,
+    },
+    workflows: {
+      checkout: `${process.env.CHECKOUT_WF_NAME || "MyCheckout2"}`,
+    },
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+
+        destination: `${process.env.SERVER_URL}/:path*`,
+      },
+    ];
+  },
+};
+
+module.exports = (phase) => {
+  const playConfig = {
+    keyId: process.env.KEY,
+    keySecret: process.env.SECRET,
+    serverUrl: `${process.env.SERVER_URL}`,
+  };
+
+  (async () => {
+    const clientPromise = orkesConductorClient(playConfig);
+    const client = await clientPromise;
+    const runner = new TaskManager(client, [returnHelloWorker()]);
+    runner.startPolling();
+  })();
+
+  console.log("Starting up ", phase);
+  return nextConfig;
+};
+      keyId: process.env.KEY,
+      keySecret: process.env.SECRET,
       serverUrl: "https://conductor-nextjs-example-ehib.vercel.app/api",
     },
     workflows: {
